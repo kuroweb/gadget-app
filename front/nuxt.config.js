f8326451a430fe2ef443dccf3faaacfbbc81b1fd
kuromitsu0104 <kuromitsu0104@gmail.com>
2020-10-20 22:10:22 +0900

    front / vuetify_module install

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
