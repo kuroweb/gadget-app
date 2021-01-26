8aafeab553b0e73762c5c257dfe2ba87a1a990f9
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-01-26 17:53:27 +0900

    ユーザー詳細ページ / リファクタリング, ユーザーのつぶやき一覧を無限スクロールで実装

--- api/app/controllers/v1/relationships_controller.rb
+++ api/app/controllers/v1/relationships_controller.rb
@@ -1,31 +1,30 @@
 class V1::RelationshipsController < ApplicationController
   before_action :set_user
 
-  # 自分自身をフォローできないように要調整
-  ## if文でcurrent_userとother_userを比較すれば良いんじゃ？
+  ################################################################################################
+  # フォローする
+  ################################################################################################
   def create
     @current_user.follow(@other_user)
     @current_user.notice_follow(@current_user.id, @other_user.id)
     render json: @other_user
   end
 
+  ################################################################################################
+  # フォローを解除する
+  ################################################################################################
   def destroy
     @current_user.unfollow(@other_user)
     render json: @other_user
   end
-  
-  # # フォロー済みかどうかを判断
-  # # リファクタリング => Userモデルに移動
-  # def isFollowed
-  #   isFollowed = @current_user.following?(@other_user)
-  #   render json: isFollowed
-  # end
 
+  ################################################################################################
+  # プライペートメソッド
+  ################################################################################################
   private
-  
-  def set_user
-    @current_user = User.find(params[:user_id])
-    @other_user = User.find(params[:follow_id])
-  end
-
+    def set_user
+      @current_user = User.find(params[:user_id])
+      @other_user = User.find(params[:follow_id])
+    end
+    
 end
---
+++ front/components/organisms/users/UserNewPosts.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-container>
+    <CreatePostDialog
+      :dialog="createPostDialog"
+      @createPost="createPost"
+      @closeDialog="createPostDialog = false"
+    />
+    <v-row>
+      <v-col cols="12" v-if="loading">
+        <v-skeleton-loader
+          class="mx-auto"
+          type="card"
+          v-for="n in 5"
+          :key="n.id"
+        />
+      </v-col>
+      <v-col cols="12" v-if="loading === false">
+        <v-row>
+          <v-col cols="12">
+            <v-btn
+              block
+              color="orange"
+              dark
+              rounded
+              @click="openCreatePostDialog"
+            >
+              新しい投稿を追加する
+            </v-btn>
+          </v-col>
+          <v-col cols="12">
+            <MicroPostCard
+              v-for="(post, index) in posts"
+              :key="index"
+              :post="post"
+            />
+            <VueInfiniteLoading
+              ref="infiniteLoading"
+              spinner="spiral"
+              @infinite="infiniteHandler"
+            >
+              <span slot="no-more">-----投稿は以上です-----</span>
+              <span slot="no-results"></span>
+            </VueInfiniteLoading>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import MicroPostCard from '~/components/organisms/MicroPostCard.vue'
+import CreatePostDialog from '~/components/organisms/posts/CreatePostDialog.vue'
+export default {
+  components: {
+    MicroPostCard,
+    CreatePostDialog
+  },
+  data () {
+    return {
+      loading: true,
+      createPostDialog: false,
+      count: 1
+    }
+  },
+  props: {
+    load: null
+  },
+  watch: {
+    async load () {
+      this.loading = true
+      this.count = 1
+      await this.$axios.$get(process.env.BROWSER_BASE_URL + `/v1/posts`, {
+        params: {
+          post_user_id: this.$store.state.modules.user.data.id,
+          page: this.count
+        }
+      })
+        .then(res => {
+          this.setPosts(res)
+          setTimeout(this.stopLoading, 500)
+        })
+    }
+  },
+  async mounted () {
+    this.count = 1
+    await this.$axios.$get(process.env.BROWSER_BASE_URL + `/v1/posts`, {
+      params: {
+        post_user_id: this.$store.state.modules.user.data.id,
+        page: this.count
+      }
+    })
+      .then(res => {
+        this.setPosts(res)
+        setTimeout(this.stopLoading, 500)
+    })
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'modules/post/posts'
+    })
+  },
+  methods: {
+    ...mapActions({
+      setPosts: 'modules/post/setPosts',
+      reloadPostsByCreatePost: 'modules/post/reloadPostsByCreatePost',
+      reloadPostsByPageScrolling: 'modules/post/reloadPostsByPageScrolling'
+    }),
+    stopLoading () {
+      this.loading = false
+    },
+    openCreatePostDialog () {
+      this.createPostDialog = true
+    },
+    createPost (payload) {
+      this.reloadPostsByCreatePost(payload)
+    },
+    async infiniteHandler () {
+      this.count += 1
+      await this.$axios.$get(process.env.BROWSER_BASE_URL + `/v1/posts`, {
+        params: {
+          post_user_id: this.$store.state.modules.user.data.id,
+          page: this.count
+        }
+      })
+        .then(res => {
+          if (res.length !== 0) {
+            setTimeout(() => {
+              this.reloadPostsByPageScrolling(res)
+              this.$refs.infiniteLoading.stateChanger.loaded()
+            }, 1000)
+          } else {
+            this.$refs.infiniteLoading.stateChanger.complete()
+          }
+        })
+    }
+  }
+}
+</script>
