35e687d1144354f218df0c4d388739729f9878e4
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-01-28 16:10:29 +0900

    faviconを変更

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'gadget-app',
+    title: 'ガジェコミ！',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
