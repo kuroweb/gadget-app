897d0b0d04da269dad5c162aa22205157aff0d47
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-02-12 18:05:35 +0900

    コメントアウトの調整

--- api/app/controllers/v1/notices_controller.rb
+++ api/app/controllers/v1/notices_controller.rb
@@ -1,5 +1,8 @@
 class V1::NoticesController < ApplicationController
 
+  ################################################################################################
+  # 通知全件取得
+  ################################################################################################
   def index
     user = User.find(params[:user_id])
     notices = user.passive_notices.includes({visitor: {avatar_attachment: :blob}},
@@ -14,12 +17,18 @@ class V1::NoticesController < ApplicationController
                                             :board])
   end
 
+  ################################################################################################
+  # 未チェック通知数を返す
+  ################################################################################################
   def unchecked
     user = User.find(params[:user_id])
     count = user.passive_notices.where(checked: false).count
     render json: count
   end
 
+  ################################################################################################
+  # 未チェック通知一覧を返す, 未チェック通知をチェック済みに変更
+  ################################################################################################
   def checked
     user = User.find(params[:user_id])
     notices = user.passive_notices.where(checked: false)
--- api/app/controllers/v1/user_tag_maps_controller.rb
+++ api/app/controllers/v1/user_tag_maps_controller.rb
@@ -1,5 +1,8 @@
 class V1::UserTagMapsController < ApplicationController
-  # タグフォロー機能
+
+  ################################################################################################
+  # タグをフォロー
+  ################################################################################################
   def create
     @follow_tag = UserTagMap.new(follow_tag_params)
     if @follow_tag.save
@@ -7,6 +10,9 @@ class V1::UserTagMapsController < ApplicationController
     end
   end
 
+  ################################################################################################
+  # タグをフォロー解除
+  ################################################################################################
   def destroy
     follow_tag = JSON.parse(params['follow_tag'])
     @followed_tag = UserTagMap.find_by(follow_tag)
@@ -14,6 +20,9 @@ class V1::UserTagMapsController < ApplicationController
     render status: 200, json: false
   end
 
+  ################################################################################################
+  # プライベートメソッド
+  ################################################################################################
   private
     def follow_tag_params
       params.require(:follow_tag).permit(:user_id, :tag_id)
