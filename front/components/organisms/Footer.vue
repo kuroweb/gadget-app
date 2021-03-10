1e2822d4cecc81581d19fc5f0a5a3dd889c7a299
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-11 01:40:29 +0900

    Update / Footerデザインの変更

--- front/components/organisms/Footer.vue
+++ front/components/organisms/Footer.vue
@@ -1,11 +1,38 @@
 <template>
   <v-footer color="cyan darken-2" dark>
-    <v-row justify="center" class="text-center" dense>
-      <v-col cols="12">
-        <n-link to="/about" class="no-link">使い方はこちら</n-link>
+    <v-row justify="center" no-gutters>
+      <v-col xl="6" lg="6" sm="6" cols="12" align-self="center" class="text-center white--text pt-2 bold">
+        <v-card flat to="/" color="cyan darken-2" dark>
+          <h1>ガジェコミ！</h1>
+          <p>ガジェット好きが集まるSNSアプリ</p>
+        </v-card>
+
       </v-col>
-      <v-col cols="12">
-        © 2021 gadget-community All Rights Reserved.
+      <v-col xl="6" lg="6" sm="6" cols="12" align-self="center" class="text-center">
+        <v-btn
+          v-for="(item, index) in items"
+          :key="index"
+          :to="item.to"
+          text
+          rounded
+        >
+          <strong>{{item.title}}</strong>
+        </v-btn>
+        <v-btn
+          v-for="(link, index) in links"
+          :key="index"
+          :href="link.to"
+          text
+          rounded
+        >
+          <strong>{{link.title}}</strong>
+        </v-btn>
+      </v-col>
+      <v-col cols="12" class="pb-5">
+        <v-divider></v-divider>
+      </v-col>
+      <v-col cols="12" align-self="center" class="text-center">
+        <p>© 2021 gadget-community All Rights Reserved.</p>
       </v-col>
     </v-row>
   </v-footer> 
@@ -13,13 +40,40 @@
   
 <script>
 export default {
-  
+  data () {
+    return {
+    }
+  },
+  computed: {
+    items() {
+      const items = [
+        {
+          title: 'ガジェコミとは？',
+          to: '/about'
+        },
+        {
+          title: '利用規約',
+          to: '/agreement'
+        }
+      ]
+      return items
+    },
+    links() {
+      const links = [
+        {
+          title: 'TWITTER',
+          to: 'https://twitter.com/kuroweb_net'
+        },
+        {
+          title: 'GITHUB',
+          to: 'https://github.com/kuromitsu0104'
+        }
+      ]
+      return links
+    }
+  }
 }
 </script>
   
 <style scoped>
-.no-link {
-  color: inherit;
-  text-decoration: none;
-}
 </style>
