743882212589a3d8cc59ad08afdf302efbcc498a
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-22 19:56:58 +0900

    [Edit] コメントを追加

--- api/app/controllers/concerns/firebase_authentication.rb
+++ api/app/controllers/concerns/firebase_authentication.rb
@@ -1,6 +1,7 @@
 module FirebaseAuthentication
   extend ActiveSupport::Concern
 
+  # トークンを検証
   def authorize
     access_token = extract_access_token_from_header
     if (extract_access_token_from_header != ENV['GUEST_UID'])
@@ -10,11 +11,13 @@ module FirebaseAuthentication
     end
   end
 
+  # トークンから取得したuidを使用して、ユーザーモデルを取得
   def current_user
     return unless @current_user_firebase_uid
     @current_user ||= User.find_by(uid: @current_user_firebase_uid)
   end
 
+  # HTTPヘッダーからトークンを抽出して検証
   def extract_access_token_from_header
     token = request.headers['Authorization'].split(" ").last
     if (token != "guest_token")
